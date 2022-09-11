466de97637b1359dc651d78bc65d1a29c499c05b
Denilson Telaroli <denilson.telaroli@pismo.io>
2022-09-11 10:11:38 -0300

    feat: patch cm to create user

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,3 +5,9 @@ kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/st
 kubectl apply -f projects/argo-application.yaml
 kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
 kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d | pbcopy
+
+# update read pass
+argocd login localhost:8888
+argocd account update-password --account read --new-password password123
+argocd account update-password --account sync --new-password password123
+argocd account update-password --account project --new-password password123
